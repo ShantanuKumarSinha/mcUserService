497d8fb9d903175367fcce664c82391b055e746e
Shantanu Kumar <shan.raj93@gmail.com>
2024-03-24 12:26:54 +0530

    refactor(mcUserSeruice): reimport dependency from org.springframework.web.bind.annotation

--- src/main/java/dev/shann/mcuserservice/controller/UserController.java
+++ src/main/java/dev/shann/mcuserservice/controller/UserController.java
@@ -6,7 +6,10 @@ import dev.shann.mcuserservice.model.Users;
 import dev.shann.mcuserservice.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
