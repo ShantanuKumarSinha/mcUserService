20c736ef1fdcaf4d19829997d07c3119ff6fbca1
Shantanu Kumar <shan.raj93@gmail.com>
2024-11-29 10:06:45 +0530

    (refactor/test) : update testCases after code refactor

--- src/test/java/dev/shann/mcuserservice/McUserServiceApplicationTests.java
+++ src/test/java/dev/shann/mcuserservice/McUserServiceApplicationTests.java
@@ -26,7 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 // to execute this rename the table user to users as user is reserved keyword
 @SpringBootTest(classes = McUserServiceApplication.class)
 @AutoConfigureMockMvc
-@TestPropertySource(locations = "file:../../../../resources/application-test.properties")
+@TestPropertySource(locations = "file:src/test/resources/application-test.properties")
 @ActiveProfiles("test")
 class McUserServiceApplicationTests {
     @Autowired
@@ -62,7 +62,7 @@ class McUserServiceApplicationTests {
 
     @Test
     @SqlGroup({
-            @Sql(scripts = "file:../../../../resources/test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+            @Sql(scripts = "file:src/test/resources/test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     })
     void shouldAuthenticateUser() throws Exception {
 
